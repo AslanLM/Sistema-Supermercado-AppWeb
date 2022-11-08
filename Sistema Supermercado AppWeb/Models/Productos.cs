700e76e34d7da10744e9348cc48ed689accc4851
LUIS GERARDO MEDINA MORENO <luis.medina2@ulatina.net>
2022-11-08 15:54:55 -0600

    Agregar archivos de proyecto.

---
+++ Sistema Supermercado AppWeb/Models/Productos.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+
+
+namespace Sistema_Supermercado_AppWeb.Models
+{
+    public partial class Productos
+    {
+        public Productos()
+        {
+            CarritoCompras = new HashSet<CarritoCompras>();
+            DetalleVenta = new HashSet<DetalleVenta>();
+            SociosComerciales = new HashSet<SociosComerciales>();
+        }
+
+        public string Id { get; set; }
+        public string Idproveedor { get; set; }
+        public string Idcategoria { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+
+        public virtual CategoriaProducto IdcategoriaNavigation { get; set; }
+        public virtual Proveedores IdproveedorNavigation { get; set; }
+        public virtual ICollection<CarritoCompras> CarritoCompras { get; set; }
+        public virtual ICollection<DetalleVenta> DetalleVenta { get; set; }
+        public virtual ICollection<SociosComerciales> SociosComerciales { get; set; }
+    }
+}
