3255f11a3bd2508a9616c8e3efa6ba583a7f2333
LUIS GERARDO MEDINA MORENO <luis.medina2@ulatina.net>
2022-11-13 23:40:25 -0600

    Put de Cliente

--- Sistema Supermercado AppWeb/Controllers/ClientesController.cs
+++ Sistema Supermercado AppWeb/Controllers/ClientesController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using Sistema_Supermercado_AppWeb.Models;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 
@@ -40,17 +42,26 @@ namespace Sistema_Supermercado_AppWeb.Controllers
         // POST: ClientesController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public async Task<ActionResult> Create(Clientes clientes)
         {
-            try
+            var httpClient = new HttpClient();
+            StringContent content = new StringContent(JsonConvert.SerializeObject(clientes), Encoding.UTF8, "appplication/json");
+            using (var response = await httpClient.PostAsync("https://localhost:000/api/", content))
+            {
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                clientes = JsonConvert.DeserializeObject<Clientes>(apiResponse);
+            }
+            if (clientes != null)
             {
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            else
             {
                 return View();
             }
         }
+
+
 
         // GET: ClientesController/Edit/5
         public ActionResult Edit(int id)
